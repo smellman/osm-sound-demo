90351967eb138c5033d7f6890372105861b217a9
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2025-07-12 09:45:50 +0900

    remove terrain support

--- src/main.ts
+++ src/main.ts
@@ -21,30 +21,10 @@ let currentLink = ""
 let currentMD5 = ""
 
 const styleUrl = "https://tile.openstreetmap.jp/styles/maptiler-toner-ja/style.json"
-const terrainUrl = "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png"
-const terrainAttribution = "ArcticDEM terrain data DEM(s) were created from DigitalGlobe, Inc., imagery and funded under National Science Foundation awards 1043681, 1559691, and 1542736; / Australia terrain data © Commonwealth of Australia (Geoscience Australia) 2017; / Austria terrain data © offene Daten Österreichs – Digitales Geländemodell (DGM) Österreich; / Canada terrain data contains information licensed under the Open Government Licence – Canada; / Europe terrain data produced using Copernicus data and information funded by the European Union - EU-DEM layers; / Global ETOPO1 terrain data U.S. National Oceanic and Atmospheric Administration / Mexico terrain data source: INEGI, Continental relief, 2016; / New Zealand terrain data Copyright 2011 Crown copyright (c) Land Information New Zealand and the New Zealand Government (All rights reserved); / Norway terrain data © Kartverket; / United Kingdom terrain data © Environment Agency copyright and/or database right 2015. All rights reserved; / United States 3DEP (formerly NED) and global GMTED2010 and SRTM terrain data courtesy of the U.S. Geological Survey."
 
 let style;
 fetch(styleUrl).then(res=> res.json()).then(json => {
   style = json
-  style['sources']['terrain'] = {
-    type: "raster-dem",
-    tiles:[terrainUrl],
-    encoding: 'terrarium',
-    tileSize: 256,
-    maxzoom: 15,
-    minzoom: 1,
-    attribution: terrainAttribution
-  }
-  style['layers'].push({
-    id: 'hills',
-    type: 'hillshade',
-    source: 'terrain',
-    paint: { 
-        'hillshade-illumination-anchor': 'map',
-        'hillshade-exaggeration': 0.2,
-    },
-  })
   style['sky'] = {
     "sky-color": "#199EF3",
     "sky-horizon-blend": 0.5,
@@ -64,7 +44,6 @@ fetch(styleUrl).then(res=> res.json()).then(json => {
         0
     ]
   }
-
 
   const map = new Map({
     container: 'map',
